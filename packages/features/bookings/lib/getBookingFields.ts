d2c5ff52eb11ef04bb65397c55a69096ae4ebe37
Udit Takkar <udit222001@gmail.com>
2024-05-23 17:44:44 +0530

    fix: seats

--- packages/features/bookings/lib/getBookingFields.ts
+++ packages/features/bookings/lib/getBookingFields.ts
@@ -225,7 +225,7 @@ export const ensureBookingInputsHaveSystemFields = ({
       defaultLabel: "phone_number",
       type: "phone",
       name: "attendeePhoneNumber",
-      required: true,
+      required: false,
       hidden: true,
       editable: "system-but-optional",
       sources: [
